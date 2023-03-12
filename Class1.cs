d4843e45b2721b9161d19e4a6f26efac87ef9545
Nikita <98824238+niktimf@users.noreply.github.com>
2023-03-12 12:19:14 +0300

    Add files via upload

--- Class1.cs
+++ Class1.cs
@@ -1,4 +1,6 @@
-﻿namespace LABA1
+﻿using System;
+
+namespace LABA1
 {
     enum TimeFrame {
         Year,
@@ -51,23 +53,14 @@
         }
 
 
-    public class Programm
-    {
-        public static void Main(string[] args)
-        {
-
-        }
-    }
-
-
     // Вариант 3
 
 
     public class Paper
     {
-        public string PublicationTitle { set; get; }
-        public Person PublicationAuthor { set; get; }
-        public System.DateTime PublicationTime { set; get; }
+        public string PublicationTitle { get; set; }
+        public Person PublicationAuthor { get; set; }
+        public System.DateTime PublicationTime { get; set; }
 
 
         public Paper(string PublicationTitle, Person PublicationAuthor, System.DateTime PublicationTime)
@@ -100,13 +93,178 @@
             this.ResearchDuration = ResearchDuration;
             this.PublicationList = PublicationList;
         }
+        
 
-        public ResearchTeam()
-        {
-            this
+        public ResearchTeam() : this("", "", 0, new TimeFrame(), new Paper[0]) {
         }
-            
-            
+
+        public ResearchTeam(string topic, string organization, int registrationNumber) : this(topic, organization, registrationNumber, new TimeFrame(), new Paper[0]) {
+        }
+
+        public string researchTopic
+        {
+        get { return ResearchTopic; }
+        set { ResearchTopic = value; }
+        }
+
+        public string researchOrganization
+        {
+        get { return ResearchOrganization; }
+        set { ResearchOrganization = value; }
+        }
+
+        public int registrationNumber
+        {
+        get { return RegistrationNumber; }
+        set { RegistrationNumber = value; }
+        }
+
+        public TimeFrame researchDuration
+        {
+        get { return ResearchDuration; }
+        set { ResearchDuration = value; }
+        }
+
+        public Paper[] publicationList
+        {
+        get { return PublicationList; }
+        set { PublicationList = value; }
+        }
+        
+        public Paper LatestPublication
+        {
+            get
+            {
+                if (PublicationList == null || PublicationList.Length == 0)
+                {
+                    return null;
+                }
+                Paper latest = PublicationList[0];
+                foreach (Paper p in PublicationList)
+                {
+                    if (p.PublicationDate > latest.PublicationDate)
+                    {
+                        latest = p;
+                    }
+                }
+                return latest;
+            }
+        }
+
+        public bool this[TimeFrame timeFrame]
+        {
+            get { return duration == timeFrame; }
+        }
+
+        public void AddPapers(params Paper[] papers)
+        {
+            if (publications == null)
+            {
+                publications = papers;
+            }
+            else
+            {
+                int n = publications.Length;
+                Array.Resize(ref publications, n + papers.Length);
+                Array.Copy(papers, 0, publications, n, papers.Length);
+            }
+        }
+
+        public override string ToString()
+        {
+            string pubStr = publications == null || publications.Length == 0
+            ? "No publications"
+            : string.Join("; ", publications);
+            return $"Topic: {topic}; Organization: {organization}; Registration number: {registrationNumber}; Duration: {duration}; Publications: {pubStr}";
+        }   
+
+        public virtual string ToShortString()
+        {
+            return $"Topic: {topic}; Organization: {organization}; Registration number: {registrationNumber}; Duration: {duration}";
+        }
+        
+
+        class Programm 
+        {
+            static void Main(string[] args)
+            {   
+                //1
+                ResearchTeam team = new ResearchTeam("IT", "Team1", 11111, TimeFrame.TwoYears);
+                Console.WriteLine(team.ToShortString());
+                
+                //2
+                Console.WriteLine(team[TimeFrame.Year]);
+                Console.WriteLine(team[TimeFrame.TwoYears]);
+                Console.WriteLine(team[TimeFrame.Long]);
+
+                //3
+                team.ResearchTopic = "Topic";
+                team.ResearchOrganization = "Organization";
+                team.RegistrationNumber = 111;
+                team.ResearchDuration = TimeFrame.Long;
+                team.AddPapers(new Paper("Title 3", new Person("First 3", "Last 3", new DateTime(2000, 1, 1)), new DateTime(2000, 1, 1)));
+                Console.WriteLine(team.ToString());
+
+                //4
+                team.AddPapers(new Paper("Title 2", new Person("First 2", "Last 2", new DateTime(2000, 1, 1)), new DateTime(2000, 1, 1)),
+                               new Paper("Title 3", new Person("First 3", "Last 3", new DateTime(2000, 1, 1)), new DateTime(2000, 1, 1)));
+                Console.WriteLine(team.ToString());
+
+                //5
+                Console.WriteLine(team.LatestPublication);
+
+                //6
+                int n = 1000000;
+                Paper[] papers1 = new Paper[n];
+                Stopwatch stopwatch1 = new Stopwatch();
+                stopwatch1.Start();
+                for (int i = 0; i < n; i++)
+                {
+                papers1[i] = new Paper($"Title {i}", new Person($"First {i}", $"Last {i}", new DateTime(2000, 1, 1)), new DateTime(2000, 1, 1));
+                }
+                stopwatch1.Stop();
+                Console.WriteLine($"One-dimensional array: {stopwatch1.ElapsedMilliseconds} ms");
+
+                // Операции с двумерным прямоугольным массивом Paper
+                int rows = 1000;
+                int cols = 1000;
+                Paper[,] papers2 = new Paper[rows, cols];
+                Stopwatch stopwatch2 = new Stopwatch();
+                stopwatch2.Start();
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < cols; j++)
+                    {
+                    papers2[i, j] = new Paper($"Title {i} {j}", new Person($"First {i} {j}", $"Last {i} {j}", new DateTime(2000, 1, 1)), new DateTime(2000, 1, 1));
+                    }
+                }
+                stopwatch2.Stop();
+                Console.WriteLine($"Two-dimensional rectangular array: {stopwatch2.ElapsedMilliseconds} ms");
+
+                // Операции с двумерным ступенчатым массивом Paper
+                int[][] papers3 = new int[rows][];
+                for (int i = 0; i < rows; i++)
+                {
+                    papers3[i] = new int[cols];
+                }
+                Stopwatch stopwatch3 = new Stopwatch();
+                stopwatch3.Start();
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < cols; j++)
+                    {
+                        papers3[i][j] = new Paper($"Title {i} {j}", new Person($"First {i} {j}", $"Last {i} {j}", new DateTime(2000, 1, 1)), new DateTime(2000, 1, 1));
+                    }
+                }
+                
+
+
+            }
+
+        }
+
+    }   
+    }     
     }
     }
     }
